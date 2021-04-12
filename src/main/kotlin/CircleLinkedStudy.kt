f84fc94b042fd8e513f8f94a15e9fa093ecf17e6
=v200123 <=295799682@qq.com>
2021-04-12 18:08:53 +0800

    循环链表

---
+++ src/main/kotlin/CircleLinkedStudy.kt
@@ -0,0 +1,16 @@
+﻿/**
+ * 双向的循环链表
+ */
+
+fun main(args: Array<String>) {
+
+}
+
+class dobleNodeList(){
+
+
+
+
+}
+
+class doubleCicleNode(var mFront : doubleCicleNode?, var mNear : doubleCicleNode? , var id :Int ,var nickName:String,var name: String)
