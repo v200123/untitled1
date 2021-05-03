97406a030da5ac9c8dcbb5147efa2add026ebd03
v200123 <295799682@qq.com>
2021-05-03 17:17:00 +0800

    完成简单版的逆波兰

---
+++ src/main/kotlin/PolandNotation.kt
@@ -0,0 +1,30 @@
+﻿import java.util.Stack
+
+fun main() {
+
+    val stack = Stack<String>()
+    //计算 5+4*6+9
+    val arrayListOf = arrayListOf<String>("4", "6", "*", "5", "+", "9", "+")
+    arrayListOf.forEach {
+        if(Regex("[0-9]").matches(it))
+        {
+            stack.push(it)
+        }else{
+            val num1 = stack.pop().toInt()
+            val num2 = stack.pop().toInt()
+            var result = 0
+            when(it){
+                "*"->{
+                    result = num1*num2
+                }
+
+                "+"->{
+                    result = num1+num2
+                }
+
+            }
+            stack.push(result.toString())
+        }
+    }
+    println("当前计算结果为:${stack.pop()}")
+}
